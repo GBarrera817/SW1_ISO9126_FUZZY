2337c5a8e80e7b13e4a18c35e048cccf4b4d9015
gonen09 <gonen.rt@gmail.com>
2017-06-29 12:03:17 -0400

    Agregadas clases metricas de las caracteristicas para leer formato JSON

---
+++ SW1_ISO9126_FUZZY/JSON/JUsabilidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SW1_ISO9126_FUZZY.JSON
+{
+    public class JUsabilidad
+    {
+        private string perspectiva;
+        private JMetrica[] comprensibilidad;
+        private JMetrica[] aprendizaje;
+        private JMetrica[] operabilidad;
+        private JMetrica[] atractividad;
+        private JMetrica[] cumplimientoUsabilidad;
+
+        public JUsabilidad() { }
+
+        public string Perspectiva { get => perspectiva; set => perspectiva = value; }
+        public JMetrica[] Comprensibilidad { get => comprensibilidad; set => comprensibilidad = value; }
+        public JMetrica[] Aprendizaje { get => aprendizaje; set => aprendizaje = value; }
+        public JMetrica[] Operabilidad { get => operabilidad; set => operabilidad = value; }
+        public JMetrica[] Atractividad { get => atractividad; set => atractividad = value; }
+        public JMetrica[] CumplimientoUsabilidad { get => cumplimientoUsabilidad; set => cumplimientoUsabilidad = value; }
+    }
+}

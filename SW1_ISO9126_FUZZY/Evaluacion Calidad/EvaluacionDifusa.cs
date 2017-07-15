eb7b1b9cc289a78f1a9c71642abaf005595a986d
Gabriela Barrera <g.barrera.angel@gmail.com>
2017-07-15 18:11:17 -0400

    Reestructuración Namespace Evaluación

---
+++ SW1_ISO9126_FUZZY/Evaluacion Calidad/EvaluacionDifusa.cs
@@ -0,0 +1,47 @@
+﻿using SW1_ISO9126_FUZZY.Logica_Difusa;
+using SW1_ISO9126_FUZZY.InferenciaDifusa;
+using System;
+using System.Collections.Generic;
+
+namespace SW1_ISO9126_FUZZY.Evaluacion_Calidad
+{
+	public static class EvaluacionDifusa
+	{
+		/// <summary>
+		/// Realiza una evaluación con lógica difusa. Los datos entregados deben tener una importancia
+		/// </summary>
+		/// <param name="datos"></param>
+		/// <param name="variables"></param>
+		/// <param name="reglas"></param>
+		/// <returns></returns>
+		public static double Evaluacion(Dictionary<string, Tuple<double, double>> datos, List<VariableLinguistica> variables, Dictionary<string, string> reglas)
+		{
+			Inferencia inferencia = new Inferencia();
+
+			foreach (VariableLinguistica variable in variables)
+				inferencia.AgregarVariable(variable);
+
+			foreach (KeyValuePair<string, string> regla in reglas)
+				inferencia.AgregarRegla(regla.Key, regla.Value);
+
+			return inferencia.EjecutarInferencia(datos);
+		}
+
+		public static double Evaluacion(Dictionary<string, double> datos, List<VariableLinguistica> variables, Dictionary<string, string> reglas)
+		{
+			Inferencia inferencia = new Inferencia();
+
+			foreach (VariableLinguistica variable in variables)
+			{
+				inferencia.AgregarVariable(variable);
+			}
+
+			foreach (KeyValuePair<string, string> regla in reglas)
+			{
+				inferencia.AgregarRegla(regla.Key, regla.Value);
+			}
+
+			return inferencia.EjecutarInferencia(datos);
+		}
+	}
+}

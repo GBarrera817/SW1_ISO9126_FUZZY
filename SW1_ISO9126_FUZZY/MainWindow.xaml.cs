50e9d4cce2fd32856d2f08a9530cbbc6c388a492
gonen09 <gonen.rt@gmail.com>
2017-07-17 01:13:25 -0400

    Creado eventos botonoes gui principal

--- SW1_ISO9126_FUZZY/MainWindow.xaml.cs
+++ SW1_ISO9126_FUZZY/MainWindow.xaml.cs
@@ -5,37 +5,102 @@ using System.Windows.Navigation;
 using MahApps.Metro.Controls;
 using SW1_ISO9126_FUZZY.Vistas;
 using MenuItem = SW1_ISO9126_FUZZY.ModelosVistas.MenuItem;
+using SW1_ISO9126_FUZZY.Modelo_Datos;
 
 namespace SW1_ISO9126_FUZZY {
+
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow {
-        public MainWindow() {
+
+    public partial class MainWindow
+    {
+        private MainPage principal;
+        private RegistroSWPage registro;
+        private VistaPreviaSeleccionMetricaPage previaSeleccion;
+        private SeleccionMetricasPage seleccion;
+        private FormularioEvaluacionPage previaEvaluacion;
+        private FormEvaluacionPage cuestionario;
+        private EvaluacionCalidadPage calidad;
+        private AcercaPage acerca;
+
+        private Evaluacion nuevaEvaluacion;
+
+
+        public MainWindow()
+        {
             InitializeComponent();
+            inicializarPaginas();
 
             // Navigate to the home page.
             Navigation.Navigation.Frame = new Frame(); //SplitViewFrame;
             Navigation.Navigation.Frame.Navigated += SplitViewFrame_OnNavigated;
-            this.Loaded += (sender, args) => Navigation.Navigation.Navigate(new MainPage());
+            this.Loaded += (sender, args) => Navigation.Navigation.Navigate(principal);
         }
 
-        private void SplitViewFrame_OnNavigated(object sender, NavigationEventArgs e) {
+
+        private void inicializarPaginas()
+        {
+            principal = new MainPage();
+            registro = new RegistroSWPage();
+            previaSeleccion = new VistaPreviaSeleccionMetricaPage();
+            seleccion = new SeleccionMetricasPage();
+
+            previaEvaluacion = new FormularioEvaluacionPage();
+            cuestionario = new FormEvaluacionPage();
+            calidad = new EvaluacionCalidadPage();
+            acerca = new AcercaPage();
+        }
+
+
+        private void SplitViewFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
             HamburgerMenuControl.Content = e.Content;
-           // btnAtrás.Visibility = Navigation.Navigation.Frame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
+            //btnAtrás.Visibility = Navigation.Navigation.Frame.CanGoBack ? Visibility.Visible : Visibility.Collapsed;
         }
 
-        private void HamburgerMenuControl_OnItemClick(object sender, ItemClickEventArgs e) {
+        private void HamburgerMenuControl_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = e.ClickedItem as MenuItem;
 
-			var menuItem = e.ClickedItem as MenuItem;
+            //if (menuItem != null && menuItem.IsNavigation)
+            //{
+            //    Navigation.Navigation.Navigate(menuItem.NavigationDestination);
+            //}
 
-            if (menuItem != null && menuItem.IsNavigation) {
-                Navigation.Navigation.Navigate(menuItem.NavigationDestination);
+            if (menuItem.Text == "Inicio")
+            {
+                Navigation.Navigation.Navigate(principal);
             }
-            
+
+            if (menuItem.Text == "Registro de Software")
+            {             
+                Navigation.Navigation.Navigate(registro);
+            }
+
+            if (menuItem.Text == "Selección de Métricas")
+            {
+                Navigation.Navigation.Navigate(previaSeleccion);
+            }
+
+            if (menuItem.Text == "Formulario de Evaluación")
+            {
+                Navigation.Navigation.Navigate(previaEvaluacion);
+            }
+
+            if (menuItem.Text == "Evaluación de Calidad")
+            {
+                Navigation.Navigation.Navigate(calidad);
+            }
+
+            if (menuItem.Text == "Acerca")
+            {
+                Navigation.Navigation.Navigate(acerca);
+            }
+
         }
 
-        /*
+        /* 
         private void IrAtras_OnClick(object sender, RoutedEventArgs e) {
             Navigation.Navigation.GoBack();
         }

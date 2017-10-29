92b56a25a08c659673a93f1b1f980b3055cf6729
gonen09 <gonen.rt@gmail.com>
2017-10-29 16:34:22 -0300

    Agregadas clase base guardar datos metricas usuario

---
+++ SW1_ISO9126_FUZZY/Modelo Datos/MTSeleccion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1_ISO9126_FUZZY.Modelo_Datos
+{
+    public class MTSeleccion
+    {
+        private int id;
+        private int proposito;
+        private bool estado;
+
+        public MTSeleccion() {}
+
+        public int Id { get => id; set => id = value; }
+        public int Proposito { get => proposito; set => proposito = value; }
+        public bool Estado { get => estado; set => estado = value; }
+    }
+}

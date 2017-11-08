e0457058a6a8963a81e0ff8c36bcac74728ec1bf
gonen09 <gonen.rt@gmail.com>
2017-11-08 00:56:08 -0300

    Actualizada clase EstadoFinal

--- SW1_ISO9126_FUZZY/Modelo Datos/EstadoFinal.cs
+++ SW1_ISO9126_FUZZY/Modelo Datos/EstadoFinal.cs
@@ -8,23 +8,17 @@ namespace SW1_ISO9126_FUZZY.Modelo_Datos
 {
     public class EstadoFinal
     {
-        private bool calidadSubcaracteristicasInterna;
-        private bool calidadSubcaracteristicasExterna;
         private bool calidadCaracteristicasInterna;
         private bool calidadCaracteristicasExterna;
         private bool calidadFinal;
 
         public EstadoFinal()
         {
-            this.CalidadSubcaracteristicasInterna = false;
-            this.CalidadSubcaracteristicasExterna = false;
             this.CalidadCaracteristicasInterna = false;
             this.CalidadCaracteristicasExterna = false;
             this.CalidadFinal = false;
         }
 
-        public bool CalidadSubcaracteristicasInterna { get => calidadSubcaracteristicasInterna; set => calidadSubcaracteristicasInterna = value; }
-        public bool CalidadSubcaracteristicasExterna { get => calidadSubcaracteristicasExterna; set => calidadSubcaracteristicasExterna = value; }
         public bool CalidadCaracteristicasInterna { get => calidadCaracteristicasInterna; set => calidadCaracteristicasInterna = value; }
         public bool CalidadCaracteristicasExterna { get => calidadCaracteristicasExterna; set => calidadCaracteristicasExterna = value; }
         public bool CalidadFinal { get => calidadFinal; set => calidadFinal = value; }

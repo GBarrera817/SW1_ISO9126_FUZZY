2afb6c0448855c7d89bdb863954179c4a1de722a
gonen09 <gonen.rt@gmail.com>
2017-11-09 17:15:53 -0300

    Agregada carpeta estados

---
+++ SW1_ISO9126_FUZZY/Modelo Datos/Estados/EstadoFinal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1_ISO9126_FUZZY.Modelo_Datos.Estados
+{
+    /// <summary>
+    ///  Estado evaluacion de calidad
+    /// </summary>
+
+    public class EstadoFinal
+    {
+        private bool calidadCaracteristicasInterna;
+        private bool calidadCaracteristicasExterna;
+        private bool calidadFinal;
+
+        public EstadoFinal()
+        {
+            this.CalidadCaracteristicasInterna = false;
+            this.CalidadCaracteristicasExterna = false;
+            this.CalidadFinal = false;
+        }
+
+        public bool CalidadCaracteristicasInterna { get => calidadCaracteristicasInterna; set => calidadCaracteristicasInterna = value; }
+        public bool CalidadCaracteristicasExterna { get => calidadCaracteristicasExterna; set => calidadCaracteristicasExterna = value; }
+        public bool CalidadFinal { get => calidadFinal; set => calidadFinal = value; }
+    }
+}

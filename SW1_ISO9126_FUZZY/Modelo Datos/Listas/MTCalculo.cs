2e0fbea583bafcf984b3a24c039784adf2051214
gonen09 <gonen.rt@gmail.com>
2017-11-10 19:23:45 -0300

    Creada la carpeta listas

---
+++ SW1_ISO9126_FUZZY/Modelo Datos/Listas/MTCalculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1_ISO9126_FUZZY.Modelo_Datos.Listas
+{
+    /// <summary>
+    /// Metrica seleccion: Guarda el calculo (datos) de una metrica
+    /// </summary>
+
+    public class MTCalculo
+    {
+        private int id;
+        private double resultado;
+
+        public MTCalculo(int id, double resultado)
+        {
+            this.Id = 0;
+            this.Resultado = 0;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public double Resultado { get => resultado; set => resultado = value; }
+    }
+}
---
+++ SW1_ISO9126_FUZZY/Modelo Datos/Listas/MTEvaluacion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW1_ISO9126_FUZZY.Modelo_Datos.Listas
+{
+    /// <summary>
+    ///  Metricas Evaluacion: Guarda la evaluacion (datos) de una metrica
+    /// </summary>
+
+    public class MTEvaluacion
+    {
+        private int id;
+        private string formula;
+        private string[] parametros;
+        private float[] valores;
+        private int mejorValor;
+        private int peorValor;
+
+        public MTEvaluacion() { }
+
+        public int Id { get => id; set => id = value; }
+        public string Formula { get => formula; set => formula = value; }
+        public string[] Parametros { get => parametros; set => parametros = value; }
+        public float[] Valores { get => valores; set => valores = value; }
+        public int MejorValor { get => mejorValor; set => mejorValor = value; }
+        public int PeorValor { get => peorValor; set => peorValor = value; }
+    }
+}

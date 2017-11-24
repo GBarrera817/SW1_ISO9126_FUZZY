030678ce84931dcae4d0fe0d0334402c95c34e39
gonen09 <gonen.rt@gmail.com>
2017-11-24 07:55:59 -0300

    Guardadas listas seleccion

--- SW1_ISO9126_FUZZY/Vistas/VistaPreviaSeleccionMetricaPage.xaml.cs
+++ SW1_ISO9126_FUZZY/Vistas/VistaPreviaSeleccionMetricaPage.xaml.cs
@@ -568,6 +568,7 @@ namespace SW1_ISO9126_FUZZY.Vistas
 
             if (caracteristica.Equals("Funcionalidad") && perspectiva.Equals("Interna"))
             {
+                miEvaluacion.Seleccion.FuncionalidadInterna = new List<MTSeleccion>(MTSfuncionalidadInterna);
                 miEvaluacion.EtiquetasSeleccion.FuncionalidadInterna.cambiarEstado(3);
                 cambiarEtiquetaGraficaEstado(miEvaluacion.EtiquetasSeleccion.FuncionalidadInterna, lblEstadoMetricasFuncInterna);
 
@@ -578,30 +579,35 @@ namespace SW1_ISO9126_FUZZY.Vistas
 
             if (caracteristica.Equals("Usabilidad") && perspectiva.Equals("Interna"))
             {
+                miEvaluacion.Seleccion.UsabilidadInterna = new List<MTSeleccion>(MTSusabilidadInterna);
                 miEvaluacion.EtiquetasSeleccion.UsabilidadInterna.cambiarEstado(3);
                 cambiarEtiquetaGraficaEstado(miEvaluacion.EtiquetasSeleccion.UsabilidadInterna, lblEstadoMetricasUsabInterna);
             }
 
             if (caracteristica.Equals("Mantenibilidad") && perspectiva.Equals("Interna"))
             {
+                miEvaluacion.Seleccion.MantenibilidadInterna = new List<MTSeleccion>(MTSmantenibilidadInterna);
                 miEvaluacion.EtiquetasSeleccion.MantenibilidadInterna.cambiarEstado(3);
                 cambiarEtiquetaGraficaEstado(miEvaluacion.EtiquetasSeleccion.MantenibilidadInterna, lblEstadoMetricasMantInterna);
             }
 
             if (caracteristica.Equals("Funcionalidad") && perspectiva.Equals("Externa"))
             {
+                miEvaluacion.Seleccion.FuncionalidadExterna = new List<MTSeleccion>(MTSfuncionalidadExterna);
                 miEvaluacion.EtiquetasSeleccion.FuncionalidadExterna.cambiarEstado(3);
                 cambiarEtiquetaGraficaEstado(miEvaluacion.EtiquetasSeleccion.FuncionalidadExterna, lblEstadoMetricasFuncExterna);
             }
 
             if (caracteristica.Equals("Usabilidad") && perspectiva.Equals("Externa"))
             {
+                miEvaluacion.Seleccion.UsabilidadExterna = new List<MTSeleccion>(MTSusabilidadExterna);
                 miEvaluacion.EtiquetasSeleccion.UsabilidadExterna.cambiarEstado(3);
                 cambiarEtiquetaGraficaEstado(miEvaluacion.EtiquetasSeleccion.UsabilidadExterna, lblEstadoMetricasUsabExterna);
             }
 
             if (caracteristica.Equals("Mantenibilidad") && perspectiva.Equals("Externa"))
             {
+                miEvaluacion.Seleccion.MantenibilidadExterna = new List<MTSeleccion>(MTSmantenibilidadExterna);
                 miEvaluacion.EtiquetasSeleccion.MantenibilidadExterna.cambiarEstado(2);
                 cambiarEtiquetaGraficaEstado(miEvaluacion.EtiquetasSeleccion.MantenibilidadExterna, lblEstadoMetricasMantExterna);
             }

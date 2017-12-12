89487d1e6ad7b1addce61a2fe47fae0617290ac1
gonen09 <gonen.rt@gmail.com>
2017-12-11 23:32:59 -0300

    Eliminadas dependencias innecesarias clases carpeta importancia

--- SW1_ISO9126_FUZZY/Modelo Datos/Importancias/Importancia.cs
+++ SW1_ISO9126_FUZZY/Modelo Datos/Importancias/Importancia.cs
@@ -1,10 +1,4 @@
-﻿using SW1_ISO9126_FUZZY.Modelo_Datos.Estados;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SW1_ISO9126_FUZZY.Modelo_Datos.Importancias
 {
     /// <summary>
